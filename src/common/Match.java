37cbf1880b040b50a8b0d37bea3fb439d5d93889
AlexProv <alex.provencher@usherbrooke.ca>
2014-09-30 19:44:36 -0400

    match update

--- src/common/Match.java
+++ src/common/Match.java
@@ -1,41 +1,130 @@
 package common;
-
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class Match {
+public class Match implements Runnable{
 	private String equipeV;
 	private String equipeD;
 	private int butV;
 	private int butD;
 	private int numPeriode;
-	private double tempsRestantPeriode;
+	private Timer timerPeriode;
+	private Timer timerAlert;
+	
+	private long tempsPeriodeMillSeconde; 
+	private long stopTime;
+	
 	private ArrayList<But> listeBut;
 	private ArrayList<Penalite> listePenalite;
-	private int numero;
+	private int id;
 	
 	
 	public Match(int numeroMatch, String equipeVis, String equipeDom){
 		equipeV = equipeVis;
 		equipeD = equipeDom;
-		numero = numeroMatch;
+		id = numeroMatch;
 		numPeriode = 1;
 		butV = 0;
 		butD = 0;
-		tempsRestantPeriode = 20;
+		timerPeriode = new Timer();
 		listeBut = new ArrayList<But>();
 		listePenalite = new ArrayList<Penalite>();
+		
 	}
 	
-	public Match(MatchSimp matchSimp, int numero){
+	public Match(MatchSimp matchSimp, int id){
 		equipeV = matchSimp.getAwayTeam();
 		equipeD = matchSimp.getHomeTeam();
-		this.numero = numero;
+		this.id = id;
 		numPeriode = 1;
-		tempsRestantPeriode = 20;
 		listeBut = new ArrayList<But>();
 		listePenalite = new ArrayList<Penalite>();
+		
+		tempsPeriodeMillSeconde = 0;
+		stopTime =  System.currentTimeMillis();
+		
+		timerPeriode = new Timer();
+		PeriodeTimer periodeTimer = new PeriodeTimer(this);
+		timerPeriode.scheduleAtFixedRate(periodeTimer, 20*60*1000,20*60*1000);
+		
+		timerAlert = new Timer();
+		AlertTimer alertTimer = new AlertTimer(this);
+		timerPeriode.scheduleAtFixedRate(periodeTimer, 2*60*1000,2*60*1000);
+				
 	}
 	
+	public String getEquipeV() {
+		return equipeV;
+	}
+
+	public void setEquipeV(String equipeV) {
+		this.equipeV = equipeV;
+	}
+
+	public String getEquipeD() {
+		return equipeD;
+	}
+
+	public void setEquipeD(String equipeD) {
+		this.equipeD = equipeD;
+	}
+
+	public int getButV() {
+		return butV;
+	}
+
+	public void setButV(int butV) {
+		this.butV = butV;
+	}
+
+	public int getButD() {
+		return butD;
+	}
+
+	public void setButD(int butD) {
+		this.butD = butD;
+	}
+
+	public int getNumPeriode() {
+		return numPeriode;
+	}
+
+	public void setNumPeriode(int numPeriode) {
+		this.numPeriode = numPeriode;
+	}
+
+
+	public ArrayList<But> getListeBut() {
+		return listeBut;
+	}
+
+	public void setListeBut(ArrayList<But> listeBut) {
+		this.listeBut = listeBut;
+	}
+
+	public ArrayList<Penalite> getListePenalite() {
+		return listePenalite;
+	}
+
+	public void setListePenalite(ArrayList<Penalite> listePenalite) {
+		this.listePenalite = listePenalite;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void alert()
+	{
+		//todo Alert Client trough observator ?
+	}
+		
 	public void ajouterPenalite(String equipe, int periode, double tempsPeriode, int tempsPenalite){
 		listePenalite.add(new Penalite(equipe, periode, tempsPeriode, tempsPenalite));
 	}
@@ -48,11 +137,6 @@ public class Match {
 			++butV;
 	}
 	
-	public int getId(){
-		return numero;
-	}
-	
-	
 	public String ToXml()
 	{
 		return SerializateurXML.objectToXML(this);
@@ -62,4 +146,52 @@ public class Match {
 	{
 		return (Match)SerializateurXML.xmlToObject(s);
 	}
+	
+	@Override
+	public void run() {
+		tempsPeriodeMillSeconde += System.currentTimeMillis() - stopTime;
+		stopTime = System.currentTimeMillis();
+	}
+	
+
+	private class PeriodeTimer extends TimerTask{
+		
+		public int nbPeriode;
+		Match match;
+		public PeriodeTimer(Match m)
+		{
+			match = m;
+		}
+		
+		@Override
+		public void run() {
+			nbPeriode +=1;
+			match.setNumPeriode(nbPeriode);
+			if(nbPeriode >= 3)
+				this.cancel();
+		}
+	
+	}
+	
+	private class AlertTimer extends TimerTask{
+		
+		public int nbAlert;
+		Match match;
+		public AlertTimer(Match m)
+		{
+			match = m;
+		}
+		
+		@Override
+		public void run() {
+			nbAlert +=1;
+			match.alert();
+			if(nbAlert >=10)
+				this.cancel();
+		}
+	
+	} 
 }
+
+
+
